87e0a5b61157d3bc34bcac76a00c03c5459062af
Jhonnatan Meza <jhonnatanmc@gmail.com>
2023-05-19 00:14:15 -0500

    se variables

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv/config"; //se importa dotenv para usar las variables 
 
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
   host: process.env.DB_HOST,
-  port: '3306',
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   define: {
     timestamps: false
